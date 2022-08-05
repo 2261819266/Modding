3e0c0d6c921ac6395c1f7c8240dafe7353f311f3
yuxinhao <2261819266@qq.com>
2022-08-05 21:17:07 +0800

    MASTER

--- Boson/src/main/java/com/tutorial/boson/food/ObsidianApple.java
+++ Boson/src/main/java/com/tutorial/boson/food/ObsidianApple.java
@@ -7,7 +7,7 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
 public class ObsidianApple extends Item {
-    public static final Food FOOD = (new Food.Builder())
+    public static final Food FOOD = new Food.Builder()
         .saturation(10)
         .hunger(20)
         .effect(() -> new EffectInstance(Effects.POISON, 3 * 20, 1), 1)
