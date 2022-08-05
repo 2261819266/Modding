a04d7eee062d211fd24bb6f3552413e14f0dbe02
yuxinhao <2261819266@qq.com>
2022-08-05 17:45:24 +0800

    master

---
+++ Boson/src/main/java/com/tutorial/boson/first_item/ItemRegistry.java
@@ -0,0 +1,15 @@
+package com.tutorial.boson.first_item;
+
+// import com.electronwill.nightconfig.core.io.Utils;
+import com.tutorial.boson.Utils;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemRegistry {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Utils.MOD_ID);
+    public static final RegistryObject<Item> obsidianIngot = ITEMS.register("obsidian_ingot", ObsidianIngot::new);
+}
+
