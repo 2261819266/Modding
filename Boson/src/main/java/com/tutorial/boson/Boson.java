4097ec2bc9e2f7294c05c57d7eb3da69030a6d9b
yuxinhao <2261819266@qq.com>
2022-08-05 20:35:57 +0800

    master

--- Boson/src/main/java/com/tutorial/boson/Boson.java
+++ Boson/src/main/java/com/tutorial/boson/Boson.java
@@ -1,13 +1,18 @@
 package com.tutorial.boson;
 
-import com.tutorial.boson.first_item.ItemRegistry;
+// import com.tutorial.boson.first_item.ItemRegistry;
+// import com.tutorial.boson.food.ItemRegistry;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Utils.MOD_ID)
 public class Boson {
     public Boson() {
-        ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        // ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        com.tutorial.boson.first_item.ItemRegistry.ITEMS.register(eventBus);
+        com.tutorial.boson.food.ItemRegistry.ITEMS.register(eventBus);
     }
 }
