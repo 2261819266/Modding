82bd7fbf5e6b1c098fa2ec4760b5cf0028ac93d0
yuxinhao <2261819266@qq.com>
2022-08-07 18:25:26 +0800

    master

--- a/src/main/java/com/tutorial/boson/item/ObsidianSword.java
+++ a/src/main/java/com/tutorial/boson/item/ObsidianSword.java
@@ -1,11 +1,10 @@
 package com.tutorial.boson.item;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SwordItem;
 
 public class ObsidianSword extends SwordItem {
     public ObsidianSword() {
-        super(ModItemTier.OBSIDIAN, 3, -2.4F, new Item.Properties().group(ItemGroup.COMBAT));
+        super(ModItemTier.OBSIDIAN, 3, -2.4F, new Item.Properties().group(ModGroup.itemGroup));
     }
 }
